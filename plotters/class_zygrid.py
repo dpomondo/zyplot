f257aa351dfcb95d3df9acfce71444f902a65266
Casey <dpoyesac@gmail.com>
2015-11-18 18:25:25 -0700

    added rough version of class_zygrid show method

--- plotters/class_zygrid.py
+++ plotters/class_zygrid.py
@@ -19,7 +19,7 @@ class Zygrid:
         *kwargs:        Formatting args, passed in as a dictionary. Options
                         include:
                         True/False options:
-                        'row_flag':     
+                        'row_flag':
                                     whether *iterables are to be formatted as
                                     rows or columns.
                         'color':    whether each box is formatted with itself,
@@ -36,14 +36,14 @@ class Zygrid:
                                     optional column names
                         'column_widths':
                                     Specify how column widths are calculated.
-                                    Default is 'minimum'.
+                                    Default is 'flexible'.
                                     Options:
-                                        'minimum': uses smallest width that
-                                        fits the largest item for all coumns.
+                                        'equal': uses smallest width that
+                                        fits the largest item for all columns.
                                         'flexible': each column is as wide as
                                         needed to fit the largest item.
                                         integer: each coumn will be the largest
-                                        of either the value of 'minimum' or the
+                                        of either the value of 'equal' or the
                                         passed-in integer.
                                         NOTE: this conflicts with 'box_width'
                                         cos I am dumb.
@@ -58,7 +58,7 @@ class Zygrid:
                         'row_names':    optional row names
                         'box_width':    otherwise box_width will be set as
                                     a function of the widest item
-                        'table_padding': padding of blank spaces to the right 
+                        'table_padding': padding of blank spaces to the right
                                     and left of the formatted table
                         'table_justification':
                                     'right': table formatted to right margin
@@ -159,52 +159,51 @@ class Zygrid:
 
     def set_column_names(self, cols):
         if cols is None:
-            object.__setattr__(self, 'column_names', None)
-            #  self.column_names = []
+            object.__setattr__(self, 'column_names', [])
         else:
             if len(cols) != self.width:
                 raise ValueError("Number of column names should equal number" +
                                  " of columns")
-            #  self.column_names = cols
             object.__setattr__(self, 'column_names', cols[:])
 
     def set_row_names(self, rws):
         if rws is None:
-            object.__setattr__(self, 'row_names', None)
-            #  self.row_names = []
+            object.__setattr__(self, 'row_names', [])
         else:
             if len(rws) != self.length:
                 raise ValueError("Number of row names should equal number" +
                                  " of rows")
-            #  self.row_names = cols
             object.__setattr__(self, 'row_names', rws[:])
 
-    def return_format_func(self):
+    def return_box_format_func(self):
         if self.kwargs.get('color', None) is not None:
             return lambda x: "{}{:^{wid}}{}".format(*x, wid=self.box_width)
         else:
             return lambda x: "{:^{wid}}".format(str(x), wid=self.box_width)
 
     def min_list_size(self, iters):
-        return min(list(len(str(i)) for i in iters))
+        if len(iters) > 0:
+            return min(list(len(str(i)) for i in iters))
+        else:
+            return 0
+
+    def max_list_size(self, iters):
+        if len(iters) > 0:
+            return max(list(len(str(i)) for i in iters))
+        else:
+            return 0
 
     def min_box_width(self, iters, padding=0, color=False):
         temp_box_width = 0
         if color is False:
             for it in iters:
-                #  for i in it:
-                    #  if len(str(i)) > temp_box_width:
-                        #  temp_box_width = len(str(i))
-                temp_box_width = max(temp_box_width, self.min_list_size(it))
+                temp_box_width = max(temp_box_width, self.max_list_size(it))
         else:
             # each object it in iters should be a 3-tuple, with a color
             # code as 0, the table entry as 1, and a color clear code as 2
             for it in iters:
-                #  for i in it:
-                    #  if len(str(i[1])) > temp_box_width:
-                        #  temp_box_width = len(str(i[1]))
                 temp_box_width = max(temp_box_width,
-                                     self.min_list_size(
+                                     self.max_list_size(
                                          list(i[1] for i in it)))
         return temp_box_width + padding
 
@@ -213,15 +212,14 @@ class Zygrid:
         return self.kwargs.get('box_width',
                                self.__dict__.get('box_width',
                                                  self.minimum_box_width))
-    
+
     def get_column_widths(self):
-        """ Creates a list of ints for use in formatting columns.    """
-        #  if not hasattr(self, 'column_widths'):
+        """ Create a list of ints for use in formatting columns.    """
         if ('column_widths' not in self.kwargs.keys() and 'column_widths' not
                 in self.__dict__.keys()):
-            self.column_widths = self.kwargs.get('column_widths', 'minimum')
+            self.column_widths = self.kwargs.get('column_widths', 'flexible')
         temp_widths = []
-        if self.column_widths == 'minimum':
+        if self.column_widths == 'equal':
             for i in range(self.stop - self.start):
                 temp_widths.append(self.minimum_box_width)
         elif isinstance(self.column_widths, int):
@@ -232,13 +230,13 @@ class Zygrid:
         elif self.column_widths == 'columns':
             if self.column_names is None:
                 # this is a terrible solution and must be fixed...
+                # A proper solution shouln't change a passed-in attribute
                 self.column_widths = 'flexible'
             else:
                 for nam in self.column_names:
                     temp_widths.append(len(nam) + self.padding)
         elif self.column_widths == 'flexible':
             if self.row_flag is False:
-                #  raise NotImplementedError
                 for i in range(self.stop - self.start):
                     temp.widths.append(
                         max(len(str(zit)) for zit in self.data[i]) +
@@ -251,9 +249,113 @@ class Zygrid:
         return temp_widths
 
     def temp_return_format(self):
-        return('col_names', 'line', 'rows', 'blank')
+        return {'header':   ['col_names', 'blank', 'line'],
+                'body':     ['rows'],
+                'footer':   ['line']}
+
+    def formatting_funcs(self):
+        def col_names(_, start, stop):
+            if len(self.column_names) == 0:
+                return None
+            col_name_trim_func = self.kwargs.get('col_trim_func',
+                lambda x, y: '{:^{wid}}'.format(x[0:y], wid=y))
+            res = ' ' * self.max_list_size(self.row_names)
+            for i in range(stop - start):
+                res += col_name_trim_func(self.column_names[i],
+                                          self.__col_wid[i])
+            return res
+
+        def line(_, start, stop):
+            res = ' ' * self.max_list_size(self.row_names)
+            for i in range(stop - start):
+                res += '+'
+                res += '-' * (self.__col_wid[i] - 1)
+            return res
+
+        def rows(ind, start, stop):
+            res = ''
+            if len(self.row_names) > 0:
+                res += '{:<{wid}}'.format(self.row_names[ind],
+                                          wid=self.max_list_size(
+                                              self.row_names))
+            for i in range(stop - start):
+                res += '{:^{wid}}'.format(self.data[ind][i],
+                                          wid=self.__col_wid[i])
+            return res
+
+        def blank(_, start, stop):
+            res = ' ' * self.max_list_size(self.row_names)
+            for i in range(stop - start):
+                res += ' ' * self.__col_wid[i]
+            return res
+
+        _dic = {'col_names':    col_names,
+                'line':         line,
+                'rows':         rows,
+                'blank':        blank}
+        return _dic
 
     def return_keys(self):
         return self.kwargs.keys()
 
-    #  def 
+    def pprinter(self):
+        # call this each time to deal with changes made in the interim...
+        self.__col_wid = self.get_column_widths()
+        res = []
+        for i in range(self.length):
+            res.append('')
+            for j in range(self.width):
+                res[-1] += '{:{wid}}'.format(self.data[i][j],
+                                             wid=self.__col_wid[j])
+        for lin in res:
+            print(lin)
+
+    def format_parser(self, begin=None, end=None, jump=None):
+    #  def format_parser(self, formatter_return_func=self.temp_return_format):
+        """ Return a list of formatting functions for use on self.data.
+            """
+        self.__col_wid = self.get_column_widths()
+        #  formatter = formatter_return_func()
+        formatter = self.temp_return_format()
+        format_func_dic = self.formatting_funcs()
+
+        # make the list
+        formatters_list = []
+        # hacktastic defaults for now...
+        if begin is None:
+            begin = self.start
+        if end is None:
+            end = self.stop
+        if jump is None:
+            jump = self.step
+        while begin < end:
+            #  for ind in range(len(self.column_names)):
+            ind = 0
+            for frm in formatter['header']:
+                formatters_list.append((format_func_dic[frm],
+                                        (ind, begin,
+                                        max(begin + jump, end))))
+                ind += 1
+            for ind in range(self.length):
+                for frm in formatter['body']:
+                    formatters_list.append((format_func_dic[frm],
+                                           (ind, begin,
+                                            max(begin + jump, end))))
+            ind = 0
+            for frm in formatter['footer']:
+                formatters_list.append((format_func_dic[frm],
+                                        (ind, begin,
+                                        max(begin + jump, end))))
+                ind += 1
+            begin += jump
+        return formatters_list
+
+    def show(self):
+        funcs = self.format_parser()
+        res = []
+        for fn in funcs:
+            temp = fn[0](*fn[1])
+            if temp is not None:
+                res.append(temp)
+        for lin in res:
+            print(lin)
